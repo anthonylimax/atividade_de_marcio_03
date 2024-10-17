523eca375536aec396adb8cb5e84c63211a4a226
Anthony Lima <anthcostalima@gmail.com>
2024-10-17 11:23:57 -0300

    resolving

--- src/main/java/com/example/atividademarcio/SwaggerCorsConfig.java
+++ src/main/java/com/example/atividademarcio/SwaggerCorsConfig.java
@@ -1,3 +1,7 @@
+package com.example.atividademarcio;
+
+
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
